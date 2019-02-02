0be021bb767c1852a18e55c3d464027cefaa8905
Kathie Roy <Katherine_Roy2@student.uml.edu>
2019-02-02 16:44:35 -0500

    testing git

--- src/main/java/edu/kathieRoy/advancedjava/App.java
+++ src/main/java/edu/kathieRoy/advancedjava/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello to my entire World!" );
+        System.out.println( "Hello to my entire World!!" );
     }
 }
