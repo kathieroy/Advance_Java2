c9786620942b69d9af4c180d76d394146577efe3
Kathie Roy <Katherine_Roy2@student.uml.edu>
2019-02-16 15:20:37 -0500

    Assignment 3 with some corrections

---
+++ src/main/java/edu/kathieRoy/advancedjava/model/Interval.java
@@ -0,0 +1,9 @@
+package edu.kathieRoy.advancedjava.model;
+
+/**
+ * This enum will hold the possible Time Intervals
+ * to be used i retrieving stock prices
+ */
+public enum Interval {
+    SECOND, MINUTE, QUARTER_HOUR, HALF_HOUR, HOUR, DAY
+}
